8af20d5b16cfd05bc1f0c3c0b294dd5ffe775ef3
AumaidKh <murtaza@fourthfrontier.com>
2023-10-04 11:19:56 +0530

    Added CatalogContent

--- app/src/main/java/com/hopcape/wallpapers/presentation/screen/home/catalog/CatalogContent.kt
+++ app/src/main/java/com/hopcape/wallpapers/presentation/screen/home/catalog/CatalogContent.kt
@@ -1,12 +1,48 @@
 package com.hopcape.wallpapers.presentation.screen.home.catalog
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.hopcape.wallpapers.presentation.composables.LoadingScreen
+import com.hopcape.wallpapers.ui.theme.Jaguar
 
 /**
- * Screen showing different categories of wallpapers*/
+ * Screen showing different categories of wallpapers
+ * @param state - screen state which is catalog screen state over here*/
 @Composable
-fun CatalogContent() {
+fun CatalogContent(
+    paddingValues: PaddingValues = PaddingValues(),
+    state: CatalogScreenState = CatalogScreenState()
+) {
+    // Loading
+    if (state.loading){
+        LoadingScreen()
+        return
+    }
+    // Showing Catalogs
+    LazyColumn(
+        modifier = Modifier
+            .background(color = Jaguar)
+            .padding(
+                bottom = paddingValues.calculateBottomPadding()
+            )
+    ){
+        items(state.catalogs){
+            CatalogComponent(
+                modifier = Modifier.padding(
+                    vertical = 4.dp
+                ),
+                name = it.name,
+                wallpapers = it.items
+            )
+        }
+    }
 
 }
 
